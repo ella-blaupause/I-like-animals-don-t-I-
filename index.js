a30bb5778f52e0a03ea102517d57e282b86a2762
Ella Kempel <ella.lernt@mail.de>
2023-03-03 11:42:20 +0100

    add solution

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-export function getAnimal() {}
+export function getAnimal(animal = "default") {
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  } else if (animal === "default") {
+    return `I do not like animals at all!`;
+  } else {
+    return `I like ${animal}!`;
+  }
+}
+
+getAnimal("cats");
